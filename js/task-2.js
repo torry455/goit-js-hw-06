ce2a3e54cdd2cc06daed53da917a6e68547857fa
Viktoriia Kucher <142267781+torry455@users.noreply.github.com>
2023-12-03 15:22:16 +0200

    finished task-1,2,3

---
+++ js/task-2.js
@@ -0,0 +1,33 @@
+'use strict';
+
+class Storage {
+    #items;
+
+constructor(initialItems) {
+    this.#items = [...initialItems];
+}
+
+getItems() {
+    return this.#items;
+}
+
+addItem(newItem) {
+    this.#items.push(newItem);
+}
+
+removeItem(itemToRemove) {
+    const index = this.#items.indexOf(itemToRemove);
+    if (index !== -1) {
+    this.#items.splice(index, 1);
+    }
+}
+}
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); 
+
+storage.addItem("Droid");
+console.log(storage.getItems());
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems());
